3a3dfe4ef1bb111f0c92abce3454cc7286dc75bf
sb2501 <sboyd@csumb.edu>
2017-11-03 11:58:24 -0700

    updates with firebase auth

--- app/src/main/java/group_project/csumb/com/autitrak/LoginActivity.java
+++ app/src/main/java/group_project/csumb/com/autitrak/LoginActivity.java
@@ -1,14 +1,76 @@
 package group_project.csumb.com.autitrak;
 
+
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.*;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText email;
+    private EditText password;
+    private Button login;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_page);
+        setContentView(R.layout.login);
+
+
+
+        auth = FirebaseAuth.getInstance();
+
+        email = (EditText)findViewById(R.id.email_editText);
+        password = (EditText)findViewById(R.id.password_editText);
+        login = (Button)findViewById(R.id.login_page_button);
+
+        login.setOnClickListener(this);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser user = auth.getCurrentUser();
+    }
+
+    private void signIn(String email_txt, String password_txt)
+    {
+        auth.signInWithEmailAndPassword(email_txt, password_txt)
+            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+                        FirebaseUser user = auth.getCurrentUser();
+                        Toast.makeText(LoginActivity.this,"Login success",Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        Toast.makeText(LoginActivity.this,"Login fail: " + task.getException().getMessage(),Toast.LENGTH_LONG).show();
+                    }
+                }
+                });
+    }
+
+    @Override
+    public void onClick(View v) {
+        String email_txt = email.getText().toString().trim();
+        String password_txt = password.getText().toString().trim();
+
+        if(v.getId() == login.getId())
+        {
+            signIn(email_txt,password_txt);
+        }
+    }
 }
--- app/src/main/java/group_project/csumb/com/autitrak/MainActivity.java
+++ app/src/main/java/group_project/csumb/com/autitrak/MainActivity.java
@@ -1,14 +1,14 @@
 package group_project.csumb.com.autitrak;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import com.firebase.database.DB.DB;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
 
     Button login;
     Button signup;
@@ -16,26 +16,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_menu);
+        setContentView(R.layout.main);
 
-        login = (Button)findViewById(R.id.menu_login_bttn);
-        signup = (Button)findViewById(R.id.menu_signup_bttn);
+        login = (Button)findViewById(R.id.login_button);
+        signup = (Button)findViewById(R.id.signup_button);
 
         login.setOnClickListener(this);
         signup.setOnClickListener(this);
 
     }
 
+
     @Override
     public void onClick(View v) {
         Intent i;
-        switch(v.getId())
+        if(v.getId() == login.getId())
         {
-            case R.id.menu_login_bttn:
-                i = new Intent(this,LoginActivity.class);
-                startActivity(i);
-                break;
-
+            i = new Intent(this,LoginActivity.class);
+            startActivity(i);
+        }
+        else if(v.getId() == signup.getId())
+        {
 
         }
     }
