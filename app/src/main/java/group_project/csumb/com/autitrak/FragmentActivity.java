5ae24e7e115ecba148fde3ee89653b8d2e979f0a
sb2501 <sboyd@csumb.edu>
2017-11-09 20:08:43 -0800

    integrated firebase auth with fragments

--- app/src/main/java/group_project/csumb/com/autitrak/FragmentActivity.java
+++ app/src/main/java/group_project/csumb/com/autitrak/FragmentActivity.java
@@ -3,10 +3,13 @@ package group_project.csumb.com.autitrak;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class FragmentActivity extends android.support.v4.app.FragmentActivity{
 
     private FragmentManager fm;
+    private MainFragment mf;
+    private LoginFragment lf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,23 +28,39 @@ public class FragmentActivity extends android.support.v4.app.FragmentActivity{
             //Start activity here
             fm = getSupportFragmentManager();
 
-            MainFragment lf = new MainFragment();
+            mf = new MainFragment();
+            lf = new LoginFragment();
 
-            fm.beginTransaction().add(R.id.fragment_container,lf).commit();
+            fm.beginTransaction().add(R.id.fragment_container,mf).commit();
 
 
             //Attach listener
-            lf.setOnClickListener(new MainFragment.FragmentListener(){
+            mf.setOnClickListener(new MainFragment.FragmentListener(){
                 @Override
                 public void onClicked(View v)
                 {
                     if(v.getId() == R.id.login_fragment_button)
                     {
-                        fm.beginTransaction().replace(R.id.fragment_container,new LoginFragment()).commit();
+                        fm.beginTransaction().replace(R.id.fragment_container,lf).commit();
                     }
                     else if(v.getId() == R.id.signup_fragment_button)
                     {
 
+                    }
+                }
+            });
+
+            lf.setOnClickListener(new LoginFragment.FragmentListener(){
+
+                @Override
+                public void onSuccess(boolean in) {
+                    if(in)
+                    {
+                        Toast.makeText(FragmentActivity.this,"Welcome",Toast.LENGTH_LONG).show();
+                    }
+                    else
+                    {
+                        Toast.makeText(FragmentActivity.this,"Login failed!",Toast.LENGTH_LONG).show();
                     }
                 }
             });
--- app/src/main/java/group_project/csumb/com/autitrak/LoginFragment.java
+++ app/src/main/java/group_project/csumb/com/autitrak/LoginFragment.java
@@ -1,10 +1,18 @@
 package group_project.csumb.com.autitrak;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * Created by sboyd on 11/9/2017.
@@ -12,10 +20,65 @@ import android.view.ViewGroup;
 
 public class LoginFragment extends Fragment{
 
-    public LoginFragment(){}
+    private FragmentListener listener;
+    private FirebaseAuth auth;
+    private Button signin;
+    private EditText email;
+    private  EditText password;
+
+    public LoginFragment(){this.listener=null;}
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         // Defines the xml file for the fragment
         return inflater.inflate(R.layout.login, parent, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        auth = FirebaseAuth.getInstance();
+        signin = (Button)view.findViewById(R.id.login_page_button);
+        email = (EditText)view.findViewById(R.id.email_editText);
+        password = (EditText)view.findViewById(R.id.password_editText);
+
+        signin.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                String email_txt = email.getText().toString().trim();
+                String password_txt = password.getText().toString().trim();
+
+                auth.signInWithEmailAndPassword(email_txt, password_txt).addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            if(listener!=null)
+                            {
+                                listener.onSuccess(true);
+                            }
+                        }
+                        else
+                        {
+                            if(listener!=null)
+                            {
+                                listener.onSuccess(false);
+                            }
+                        }
+                    }
+                });
+
+            }
+        });
+    }
+
+
+    public void setOnClickListener(FragmentListener listener)
+    {
+        this.listener = listener;
+    }
+
+    public interface FragmentListener{
+        public void onSuccess(boolean in);
     }
 }
