2b39098ef290a58765e38360b7c6e4b2fae43d8e
chanel <caquino@csumb.edu>
2017-12-02 17:26:41 -0800

    blah blah

--- app/src/main/java/group_project/csumb/com/autitrak/chanel/CaregiverMainActivity.java
+++ app/src/main/java/group_project/csumb/com/autitrak/chanel/CaregiverMainActivity.java
@@ -1,5 +1,8 @@
 package group_project.csumb.com.autitrak.chanel;
 
+import android.content.Intent;
+import android.media.Image;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -13,11 +16,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
 
 import group_project.csumb.com.autitrak.R;
 
 public class CaregiverMainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    ImageButton progressBtn;
+    ImageButton notifBtn;
+    ImageButton settingsBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +35,46 @@ public class CaregiverMainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // init ImageButtons
+        progressBtn = (ImageButton) findViewById(R.id.caregiver_progress_btn);
+        notifBtn = (ImageButton) findViewById(R.id.caregiver_notif_btn);
+        settingsBtn = (ImageButton) findViewById(R.id.caregiver_settings_btn);
 
-//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.caregiver_drawer_layout);
-//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-//                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-//        drawer.setDrawerListener(toggle);
-//        toggle.syncState();
-//
-//        NavigationView navigationView = (NavigationView) findViewById(R.id.caregiver_nav_view);
-//        navigationView.setNavigationItemSelectedListener(this);
+        // listener for progressBtn
+        progressBtn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                setTitle("Progress");
+                ProgressFragment pf = new ProgressFragment();
+                FragmentManager fm = getSupportFragmentManager();
+                fm.beginTransaction().replace(R.id.caregiver_fragment, pf).addToBackStack(null).commit();
+            }
+        });
+
+        // listener for notifBtn
+        notifBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setTitle("Notifications");
+                NotificationsFragment nf = new NotificationsFragment();
+                FragmentManager fm = getSupportFragmentManager();
+                fm.beginTransaction().replace(R.id.caregiver_fragment, nf).addToBackStack(null).commit();
+            }
+        });
+
+        // listener for settingsBtn
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setTitle("Settings");
+                SettingsFragment sf = new SettingsFragment();
+                FragmentManager fm = getSupportFragmentManager();
+                fm.beginTransaction().replace(R.id.caregiver_fragment, sf).addToBackStack(null).commit();
+            }
+        });
     }
+
+
 
     @Override
     public void onBackPressed() {
