50deb3c1bee4df3643d43b30da1164b8a80a9b3f
chanel <caquino@csumb.edu>
2017-12-05 20:58:59 -0800

    inbox stuff

--- app/build/generated/source/r/debug/com/google/android/gms/R.java
+++ app/build/generated/source/r/debug/com/google/android/gms/R.java
@@ -64,11 +64,11 @@ public final class R {
         public static final int normal = 0x7f08008c;
         public static final int radio = 0x7f08009f;
         public static final int standard = 0x7f0800c9;
-        public static final int text = 0x7f0800d4;
-        public static final int text2 = 0x7f0800d5;
-        public static final int toolbar = 0x7f0800e0;
-        public static final int wide = 0x7f0800ec;
-        public static final int wrap_content = 0x7f0800ef;
+        public static final int text = 0x7f0800d3;
+        public static final int text2 = 0x7f0800d4;
+        public static final int toolbar = 0x7f0800df;
+        public static final int wide = 0x7f0800eb;
+        public static final int wrap_content = 0x7f0800ee;
     }
     public static final class integer {
         public static final int google_play_services_version = 0x7f090006;
